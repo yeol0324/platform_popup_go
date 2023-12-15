77ae075952d467d9f3d38c3de70ce0e81a7f5e8e
TedChang <ghkd7784@minigate.net>
2023-12-15 15:51:58 +0900

    updated "nest 환경변수(env), 프로파일(cross-env) 설정"

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env`, `.${process.env.NEST_ENV}.env`], // env파일 경로 설정
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- be/src/main.ts
+++ be/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
+const logger = new Logger('main.ts');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  logger.log(`NEST_ENV = ${process.env.NEST_ENV}`); // cross-env 확인
+  logger.log(`PROFILE = ${process.env.PROFILE}`); // 프로파일별 env 잘 불러오는지 확인
+
+  const port = process.env.SERVER_PORT; // 포트번호 확인
+  logger.log(`PORT = ${port}`);
+
+  await app.listen(port);
 }
 bootstrap();
