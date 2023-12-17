66052c24682d53df9f56912d2ea9d3cc93871004
yeol0324 <73329772+yeol0324@users.noreply.github.com>
2023-12-18 00:57:19 +0900

    add "Map component"

--- fe/app/map/page.tsx
+++ fe/app/map/page.tsx
@@ -3,10 +3,12 @@
 import { Suspense } from 'react';
 import Image from 'next/image';
 import Spinner from '@/components/Loading/Loading';
+import { Map } from '@/components/Map';
 export default function List() {
   return (
     <div className="map-home">
       <Suspense fallback={<Spinner />}></Suspense>
+      <Map></Map>
     </div>
   );
 }
