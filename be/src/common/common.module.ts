1ac69989fbefb7c480360c91af5d86f1f90439ab
TedChang <ghkd7784@minigate.net>
2023-12-19 09:23:40 +0900

    updated "typeorm customRepository 적용"

--- be/src/common/common.module.ts
+++ be/src/common/common.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CommonController } from './common.controller';
 import { CommonService } from './common.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmExModule } from 'src/config/db/typeorm-ex.module';
+import { ApiConnectionRepository } from './api-connection.repository';
 
 @Module({
+  imports: [TypeOrmExModule.forCustomRepository([ApiConnectionRepository])],
   controllers: [CommonController],
-  providers: [CommonService]
+  providers: [CommonService],
 })
 export class CommonModule {}
